648f02369b9e9b322089fa124c8a7624b29936e5
xom9s <xom9k321@gmail.com>
2026-03-06 23:22:07 +0100

    entity name corrected in the book repository

--- src/main/java/com/example/book_management/repositories/BookRepository.java
+++ src/main/java/com/example/book_management/repositories/BookRepository.java
@@ -1,10 +1,10 @@
 package com.example.book_management.repositories;
 
+import com.example.book_management.entities.BookEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.awt.print.Book;
 
 @Repository
-public interface bookRepository extends JpaRepository<Book, Long> {
+public interface BookRepository extends JpaRepository<BookEntity, Long> {
 }
