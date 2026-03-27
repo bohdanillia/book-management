5f2418223a2b2e76addfb5dcb4e4a552504e0cf1
illia <xom9k321@gmail.com>
2026-03-27 16:44:22 +0100

    added user dto

---
+++ src/main/java/com/example/book_management/dtos/UserDTO.java
@@ -0,0 +1,30 @@
+package com.example.book_management.dtos;
+
+
+public class UserDTO {
+
+        private int id;
+        private String email;
+        private String password;
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+
+    }
+
+
