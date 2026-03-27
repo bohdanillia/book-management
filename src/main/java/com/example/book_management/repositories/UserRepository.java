f106911d4d05ac7935e75b6972eb8aa59a389b69
illia <xom9k321@gmail.com>
2026-03-27 16:44:13 +0100

    added user repository

---
+++ src/main/java/com/example/book_management/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.book_management.repositories;
+
+import com.example.book_management.entities.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+}
