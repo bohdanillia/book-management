adcfb34a8982102ffd28273853d2ef420ac8957f
illia <xom9k321@gmail.com>
2026-03-30 22:52:04 +0200

    added validations to user dto

--- src/main/java/com/example/book_management/dtos/UserDTO.java
+++ src/main/java/com/example/book_management/dtos/UserDTO.java
@@ -1,11 +1,18 @@
 package com.example.book_management.dtos;
 
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 public class UserDTO {
 
-        private int id;
-        private String email;
-        private String password;
+    private int id;
+    @NotBlank(message = "Email cannot be empty")
+    @Email
+    private String email;
+    @NotBlank(message = "Password cannot be empty")
+
+    private String password;
 
         public int getId() {
             return id;
