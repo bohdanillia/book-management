32d551f6fe6902559629746986ce0a96afdaa7b4
illia <xom9k321@gmail.com>
2026-03-30 22:51:04 +0200

    added book dto

---
+++ src/main/java/com/example/book_management/dtos/BookDTO.java
@@ -0,0 +1,53 @@
+package com.example.book_management.dtos;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class BookDTO {
+    @NotBlank(message = "Title cannot be empty")
+    private String title;
+    private String author;
+    @Min(value = 1500, message = "Year should be more than 1500")
+    @Max(value = 2026, message = "Year should be less than 2026")
+    private int year;
+    @Size(max = 300)
+    private String description;
+
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
