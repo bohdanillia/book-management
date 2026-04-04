69a1d52dd34264e9ff68869298c1fab90ac75e87
illia <xom9k321@gmail.com>
2026-04-04 17:52:59 +0200

    returning book dtos in book service not entities

--- src/main/java/com/example/book_management/service/BookService.java
+++ src/main/java/com/example/book_management/service/BookService.java
@@ -1,11 +1,11 @@
 package com.example.book_management.service;
 
+import com.example.book_management.dtos.BookDTO;
 import com.example.book_management.entities.BookEntity;
 import com.example.book_management.repositories.BookRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class BookService {
@@ -16,25 +16,47 @@ public class BookService {
 		this.bookRepository = bookRepository;
 	}
 
-	public void addBook(BookEntity book){
-		bookRepository.save(book);
+	public BookDTO addBook(BookEntity book){
+		return toDTO(bookRepository.save(book));
 	}
 
-	public Optional<BookEntity> getBookById(Long id){
-		return bookRepository.findById(id);
+	public BookDTO getBookById(Long id){
+		BookEntity book = bookRepository.findById(id).orElseThrow(() -> new RuntimeException("Book not found"));
+		return toDTO(book);
 	}
 
-	public List<BookEntity> getAllBooks(){
-		return bookRepository.findAll();
+	public List<BookDTO> getAllBooks(){
+		return bookRepository.findAll()
+				.stream()
+				.map(this::toDTO)
+				.toList();
 	}
 
-	public BookEntity updateBook(Long id, BookEntity updatedBook){
+	public BookDTO updateBook(Long id, BookEntity updatedBook){
 		BookEntity bookToUpdate = bookRepository.findById(id).orElseThrow(() -> new RuntimeException("Book not found"));
 		bookToUpdate.setAuthor(updatedBook.getAuthor());
 		bookToUpdate.setDescription(updatedBook.getDescription());
 		bookToUpdate.setYear(updatedBook.getYear());
 
-		return bookRepository.save(bookToUpdate);
+		return toDTO(bookRepository.save(bookToUpdate));
+	}
+
+	public BookEntity toEntity(BookDTO bookDTO){
+		BookEntity bookEntity = new BookEntity();
+		bookEntity.setTitle(bookDTO.getTitle());
+		bookEntity.setAuthor(bookDTO.getAuthor());
+		bookEntity.setYear(bookDTO.getYear());
+		bookEntity.setDescription(bookDTO.getDescription());
+		return bookEntity;
+	}
+
+	public BookDTO toDTO(BookEntity bookEntity){
+		BookDTO bookDTO = new BookDTO();
+		bookDTO.setTitle(bookEntity.getTitle());
+		bookDTO.setAuthor(bookEntity.getAuthor());
+		bookDTO.setYear(bookEntity.getYear());
+		bookDTO.setDescription(bookEntity.getDescription());
+		return bookDTO;
 	}
 
 	public void deleteBookById(Long id){
