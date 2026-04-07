afa091066fa8f988e80b9501dc408ebbe2efdb82
illia <xom9k321@gmail.com>
2026-04-07 10:47:07 +0200

    added mockito test for adding book

--- src/test/java/com/example/book_management/service/BookServiceTest.java
+++ src/test/java/com/example/book_management/service/BookServiceTest.java
@@ -4,29 +4,48 @@ import com.example.book_management.dtos.BookDTO;
 import com.example.book_management.entities.BookEntity;
 import com.example.book_management.repositories.BookRepository;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import java.util.Optional;
+
 class BookServiceTest {
-    @Autowired
+    @InjectMocks
     private BookService bookService;
-    @Autowired
+    @Mock
     private BookRepository bookRepository;
 
     @Test
     public void addBookTest(){
         BookEntity entity = new BookEntity();
+        entity.setId(1L);
         entity.setTitle("Harry Potter");
         entity.setAuthor("Rowling");
         entity.setDescription("Favourite book");
         entity.setYear(1998);
 
-        BookDTO dto = bookService.toDTO(entity);
+        BookDTO dto = new BookDTO();
+        entity.setTitle("Harry Potter");
+        entity.setAuthor("Rowling");
+        entity.setDescription("Favourite book");
+        entity.setYear(1998);
+
+        Mockito.when(bookRepository.save(Mockito.any(BookEntity.class))).thenReturn(entity);
+        Mockito.when(bookRepository.findById(entity.getId())).thenReturn(Optional.of(entity));
+
         BookDTO saved = bookService.addBook(dto);
 
         Assertions.assertNotNull(saved.getId());
         Assertions.assertTrue(bookRepository.findById(saved.getId()).isPresent());
+    }
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
     }
 }
